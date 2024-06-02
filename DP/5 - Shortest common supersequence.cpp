b415625f007dc19243b6daef069d7967615dd831
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-06-02 06:26:46 +0530

    Create 5 - Shortest common supersequence.cpp

---
+++ DP/5 - Shortest common supersequence.cpp
@@ -0,0 +1,8 @@
+/*
+/  i/p =  A G G T A B
+       = G X T X A Y B
+/
+/  o/p = Jo common hai , vo ek baar likhna hai , rest ko likhna hi hoga  ( Length = 9)
+/
+/  Imp = Simply  =   m + n - LCS(str1 , str2 , m , n)
+*/
