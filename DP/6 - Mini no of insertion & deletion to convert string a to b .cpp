992f66c060ec979d3bd40e708cb052d7a0a05714
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-06-02 06:49:14 +0530

    Update 6 - Mini no of insertion & deletion to convert string a to b .cpp

--- DP/6 - Mini no of insertion & deletion to convert string a to b .cpp
+++ DP/6 - Mini no of insertion & deletion to convert string a to b .cpp
@@ -40,3 +40,37 @@ int main() {
 
 // Bottom up DP :
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int LCS(string X, string Y, int n, int m) {
+	int dp[n + 1][m + 1]; // DP - matrix
+
+	// base case of recursion --> for initialization of dp - matrix
+	for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= m; j++)
+			if (i == 0 || j == 0)
+				dp[i][j] = 0;
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			if (X[i - 1] == Y[j - 1]) // when last character is same
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+			else // when last character is not same -> pick max
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+
+	return dp[n][m];
+}
+
+int main() {
+	string X = "heap";
+	string Y = "pea";
+	
+
+	int n = X.length(), m = Y.length();
+
+	cout << "Deletion" << n - LCS(X, Y, n, m) << endl;
+	cout << "Insertion" << m - LCS(X, Y, n, m) << endl;
+	return 0;
+}
+
