1f8a26f71c29bf9eaff8896a04cdebe8a700479a
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-07-10 22:11:12 +0530

    Create 7 - Inheritance.cpp

---
+++ Fundamentals/OOPS/7 - Inheritance.cpp
@@ -0,0 +1,5 @@
+/*  
+- Process in which the properties & member function of base class are passed on to the derived class
+- Class A ( Parent / base) & Class B ( child / derived )
+- use for code reusability
+*/
