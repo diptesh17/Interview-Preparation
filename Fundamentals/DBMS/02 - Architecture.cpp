566cf870b87e2efbba59cfb4870bb439a33eb417
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-07-12 00:02:30 +0530

    Create 02 - Architecture.cpp

---
+++ Fundamentals/DBMS/02 - Architecture.cpp
@@ -0,0 +1,7 @@
+/* 
+- Three level of architecture / levels of abstraction
+
+- Physical level : lowest level & describes how data is stored
+- Logical level : describes what data is store along with design of db & relationships
+- View level : 
+*/
