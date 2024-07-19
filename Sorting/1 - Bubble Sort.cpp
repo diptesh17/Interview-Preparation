733791e2b29ed119fa8a3d49e88b8c31042c0916
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-07-19 23:15:32 +0530

    Create 1 - Bubble Sort.cpp

---
+++ Sorting/1 - Bubble Sort.cpp
@@ -0,0 +1,10 @@
+/* 
+        Remember
+
+  - Reapetedly swap two adj element if they are at wrong place
+  - After every pass = greater elem will be at its right place
+  - 1 pass = check till n-1
+  - 2 pass = check till n-2
+  - i pass = check till n-i
+
+*/
