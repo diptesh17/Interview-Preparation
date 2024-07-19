c01a78bf7ba1e495f24c6c76431845725120ca8c
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-07-19 13:24:59 +0530

    Create 15 - Pascal's Triangle.cpp

---
+++ Arrays/15 - Pascal's Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result(numRows);
+        
+        for(int i = 0; i<numRows; i++) {
+            
+            result[i] = vector<int>(i+1, 1);
+            
+            for(int j = 1; j < i; j++) {
+                
+                result[i][j] = result[i-1][j] + result[i-1][j-1];
+                
+            }
+            
+        }
+        
+        return result;
+    }
+};
