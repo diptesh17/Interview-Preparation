e851b7bc4d69e18d95aec7e1f5a9a15ff7a184c7
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-07-30 15:07:07 +0530

    Create 10 - Min Stack.cpp

---
+++ Stack and Queue/10 - Min Stack.cpp
@@ -0,0 +1,35 @@
+// Use extra stack to maintain the minimum element
+
+class MinStack {
+public:
+    
+    stack<int>st, s2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if( s2.empty() || val <= s2.top() ){
+            s2.push(val);
+        }
+        
+        st.push(val);
+    }
+    
+    void pop() {
+        
+        if(st.top() == s2.top()){
+            s2.pop();
+        }
+        st.pop();
+        
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
