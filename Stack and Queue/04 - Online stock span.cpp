eb673ec676d0889eb7086b630c5ce2f46a071640
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-08-01 08:47:35 +0530

    Create 04 - Online stock span.cpp

---
+++ Stack and Queue/04 - Online stock span.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public : 
+
+     int next(int price) {
+          int span = 1;
+          
+          while(!st.empty() && st.top().first <= price) {
+              span += st.top().second;
+              st.pop();
+          }
+          
+          st.push({price, span});
+          return span;
+      }
+};
