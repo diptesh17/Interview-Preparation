b905d6c6750baad663d0795a07727e0e731224d2
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-08-25 17:06:41 +0530

    Create 1 - Basic.cpp

---
+++ Sliding-Window/1 - Basic.cpp
@@ -0,0 +1,6 @@
+/* Identify : 
+    - Arr / string + subarray/substring + largest/min + k
+    - Sometimes k can be ask to find
+    - Fix size window : Initially we fix window size + add the next one by removeing first one from the window 
+    - Variable size : Will be ask to find window satisfying given condition
+*/
