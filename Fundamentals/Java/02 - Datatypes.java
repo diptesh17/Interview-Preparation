79f630cb78946b194d658d9b06705a2bc6709f7c
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-08-31 17:45:04 +0530

    Create 02 - Datatypes.java

---
+++ Fundamentals/Java/02 - Datatypes.java
@@ -0,0 +1,16 @@
+/* 
+- int - 4 bytes
+- float - 4 bytes
+- char - 2 bytes
+- boolean - 1 bit
+- byte - 1 byte
+- short - 2 byte
+- long - 8 byte
+- double - 8 byte
+*/
+
+// float => float pi = 3.14f
+// f not add => java treated it as a double
+
+// char => char letter = 'A';
+// string => string letter = "A" ;
